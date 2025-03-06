be8e033cb9e61bcb07835aabce2757fb857d66ad
angelvargass <59036972+angelvargass@users.noreply.github.com>
2025-03-06 10:56:24 -0600

    feat: first gin route (#9)
    * ci: add needs to build and push image job
    
    * feat: first gin route

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"log/slog"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	r := gin.Default()
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(http.StatusOK, gin.H{
+			"message": "pong",
+		})
+	})
+	err := r.Run()
+	if err != nil {
+		slog.Error(err.Error())
+	}
 }
